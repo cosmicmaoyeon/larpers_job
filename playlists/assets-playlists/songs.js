f291bf37d60ab0db5b88e0ce2c139467f8fa5004
cosmicmaoyeon <ostrowskisenffnick@gmail.com>
2026-05-16 20:57:44 -0300

    comitando

--- playlists/assets-playlists/songs.js
+++ playlists/assets-playlists/songs.js
@@ -4,13 +4,13 @@ export default [
         name: "Something Stupid",
         artist: "Frank Sinatra & Nancy Sinatra",
         cover: "https://i.scdn.co/image/ab67616d0000b2733e2331ff09f59ea3c87fe5d6",
-        lyrics: "ebaaaa\nvamotropa\n",
+        lyrics: "\nI know I stand in line\nUntil you think you have the time\nTo spend an evening with me\nAnd if we go some place to dance\nI know that there's a chance\nYou won't be leaving with me\nThen afterwards we drop into a quiet little place\nAnd have a drink or two\nAnd then I go and spoil it all\nBy saying somethin' stupid like, 'I love you'\nI can see it in your eyes\nThat you despise the same old lies\nYou heard the night before\nAnd though it's just a line to you\nFor me it's true\nAnd never seemed so right before\nI practice every day \nTo find some clever lines to say\nTo make the meaning come true\nBut then I think I'll wait\nUntil the evening gets late\nAnd I'm alone with you\nThe time is right, your perfume fills my head\nThe stars get red, and, oh, the night's so blue\nAnd then I go and spoil it all\nBy saying somethin' stupid like, 'I love you'\nThe time is right, your perfume fills my head\nThe stars get red, and, oh, the night's so blue\nAnd then I go and spoil it all\nBy saying somethin' stupid like, 'I love you'\n'I love you'\n'I love you\n'I love you'\n'I love you'",
     },
     {
         src: "assets-playlists/src/zombie.mp3",
         name: "Zombie",
         artist: "The Cranberries",
-        cover: "",
+        cover: "https://i.scdn.co/image/ab67616d0000b273e2213d84fabb15100c425198",
         lyrics: "vamo\ntabo\n",
     },
     {
